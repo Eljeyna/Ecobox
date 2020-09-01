e7c27673a7fa08b8c89ad8a9fcd41f1a57b16481
Llein <43903165+Eljeyna@users.noreply.github.com>
2020-09-01 21:46:22 +0300

    Update Spawner

--- Assets/3D/Game Manager/Spawner3D.cs
+++ Assets/3D/Game Manager/Spawner3D.cs
@@ -8,19 +8,54 @@ public class Spawner3D : MonoBehaviour
     public List<GameObject> waveEntities;
     public int currentWaveCount;
     public float checkRadius = 1f;
+    public BoxCollider spawnArea;
+    public Transform entitiesGroup;
+    public float waitingTime;
+
+    private void Start()
+    {
+        spawnArea = GetComponent<BoxCollider>();
+    }
 
     private void Update()
     {
+        if (waitingTime > Time.time)
+            return;
+
         if (spawn && currentWaveCount < GameDirector3D.maxWave)
         {
-            for (int i = 0; i < (int)Random.Range(1f, 8f); i++)
+            for (int i = 0; i < (int)Random.Range(1f, GameDirector3D.maxWave); i++)
             {
-                int random = (int)Random.Range(0f, 1f);
-                /*Collider[] checkResult = Physics.OverlapSphere(targetPosition, checkRadius);
-                if (checkResult.Length == 0)
+                int random = (int)Random.Range(0f, wavePrefabs.Length - 1);
+                int attempts = 0;
+
+                Vector3 pos = new Vector3(
+                        Random.Range(spawnArea.transform.position.x, spawnArea.transform.position.x + spawnArea.size.x),
+                        0,
+                        Random.Range(spawnArea.transform.position.z, spawnArea.transform.position.z + spawnArea.size.z)
+                );
+
+                Collider[] checkResult = Physics.OverlapSphere(pos, checkRadius);
+
+                while (checkResult.Length > 0 && attempts < 10)
                 {
-                    waveEntities.Add(Instantiate(wavePrefabs[random], ));
-                }*/
+                    pos = new Vector3(
+                        Random.Range(spawnArea.transform.position.x, spawnArea.transform.position.x + spawnArea.size.x),
+                        0,
+                        Random.Range(spawnArea.transform.position.z, spawnArea.transform.position.z + spawnArea.size.z)
+                    );
+
+                    if (checkResult.Length == 0)
+                    {
+                        waveEntities.Add(Instantiate(wavePrefabs[random], pos, Quaternion.identity, entitiesGroup));
+                        currentWaveCount++;
+                        waitingTime = Time.time + Random.Range(0f, 2f);
+                        continue;
+                    }
+
+                    checkResult = Physics.OverlapSphere(pos, checkRadius);
+                    attempts++;
+                }
             }
         }
     }
