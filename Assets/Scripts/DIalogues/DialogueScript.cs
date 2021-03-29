a36503be939f62ae28ef78145ac8910d1526d73b
Eljeyna <mironov20002000@gmail.com>
2021-03-29 10:34:01 +0300

    Commit master

--- Assets/Scripts/DIalogues/DialogueScript.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class DialogueScript : MonoBehaviour
-{
-    public abstract void Use();
-}
--- Assets/Scripts/DIalogues/InvisibleButton.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class InvisibleButton : MonoBehaviour
-{
-    public void Use()
-    {
-        GameDirector.Instance.dialogue.SetDialogue(GameDirector.Instance.dialogue._currentLine + 1);
-    }
-}
