1d5c43655e96bbc5bb9afbcfd48f30864650b7c2
Eljeyna <mironov20002000@gmail.com>
2021-04-01 22:18:55 +0300

    Update form

--- Assets/Scripts/PHP/Web.cs
+++ Assets/Scripts/PHP/Web.cs
@@ -6,6 +6,6 @@ public class Web : MonoBehaviour
     {
         //Debug.Log(StaticGameVariables.GetRequest("http://eztix/GetUsers.php"));
         Debug.Log(StaticGameVariables.UserLogin("__test__testunity@gmail.com", "121212"));
-        Debug.Log(StaticGameVariables.UserRegister("TestUnity", "mironov20002000@gmail.com", "121212"));
+        //Debug.Log(StaticGameVariables.UserRegister("TestUnity", "mironov20002000@gmail.com", "121212"));
     }
 }
