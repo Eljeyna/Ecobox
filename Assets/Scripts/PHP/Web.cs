38d1ab2ce6a7169748ef7899ec409513f418313e
Eljeyna <mironov20002000@gmail.com>
2021-04-20 08:03:13 +0300

    Fix sign in && add register

--- Assets/Scripts/PHP/Web.cs
+++ Assets/Scripts/PHP/Web.cs
@@ -4,11 +4,23 @@ using TMPro;
 
 public class Web : MonoBehaviour
 {
+    public Canvas canvasSignIn;
+    public Canvas canvasSignUp;
+
     public TMP_InputField loginField;
+    public TMP_InputField loginFieldRegister;
+
     public TMP_InputField passwordField;
+    public TMP_InputField passwordFieldRegister;
+    public TMP_InputField passwordConfirmFieldRegister;
+
+    public TMP_InputField emailRegister;
+
     public TMP_Text messageField;
 
     public Button enterLogin;
+    public Button enterRegister;
+
     public Button playOffline;
 
     public Translate translate;
@@ -17,12 +29,13 @@ public class Web : MonoBehaviour
 
     private readonly string errorLogin = "User does not exists";
     private readonly string errorPassword = "Wrong password";
+    private readonly string confirmRegisterMessage = "All done";
 
     private void Start()
     {
         //Debug.Log(StaticGameVariables.GetRequest("http://eztix/GetUsers.php"));
-        //Debug.Log(StaticGameVariables.UserRegister("TestUnity", "mironov20002000@gmail.com", "121212"));
         enterLogin.onClick.AddListener(Login);
+        enterRegister.onClick.AddListener(Register);
         playOffline.onClick.AddListener(PlayOffline);
     }
 
@@ -34,8 +47,7 @@ public class Web : MonoBehaviour
 
             return;
         }
-
-        if (string.IsNullOrWhiteSpace(passwordField.text))
+        else if (string.IsNullOrWhiteSpace(passwordField.text))
         {
             ShowMessageField("Enter password");
 
@@ -56,7 +68,42 @@ public class Web : MonoBehaviour
 
     private void Register()
     {
-        //TODO: Add register form with action (Register.php)
+        if (string.IsNullOrWhiteSpace(loginFieldRegister.text))
+        {
+            ShowMessageField("Enter login/email");
+
+            return;
+        }
+        else if (string.IsNullOrWhiteSpace(emailRegister.text))
+        {
+            ShowMessageField("Enter email");
+
+            return;
+        }
+        else if (string.IsNullOrWhiteSpace(passwordFieldRegister.text))
+        {
+            ShowMessageField("Enter password");
+
+            return;
+        }
+        else if (passwordFieldRegister.text != passwordConfirmFieldRegister.text)
+        {
+            ShowMessageField("Passwords are different");
+
+            return;
+        }
+
+        message = StaticGameVariables.UserRegister(loginFieldRegister.text, emailRegister.text, passwordFieldRegister.text);
+
+        if (message == confirmRegisterMessage)
+        {
+            canvasSignUp.enabled = false;
+            canvasSignIn.enabled = true;
+        }
+        else
+        {
+            ShowMessageField(message);
+        }
     }
 
     private void ShowMessageField(string text)
@@ -74,12 +121,14 @@ public class Web : MonoBehaviour
 
     private void PlayOnline(string ID)
     {
+        messageField.enabled = false;
         messageField.text = ID;
         SceneLoading.Instance.SwitchToScene("MainMenu", SceneLoading.startAnimationID);
     }
 
     private void PlayOffline()
     {
+        messageField.enabled = false;
         SceneLoading.Instance.SwitchToScene("MainMenu", SceneLoading.startAnimationID);
     }
 }
