dc999b02edd9a300b1e1a54717fac604eb00d919
Llein <43903165+Eljeyna@users.noreply.github.com>
2021-04-24 13:40:54 +0300

    Update

--- Assets/Scripts/Quests/QuestsInitializers/NewBeginnings.cs
+++ Assets/Scripts/Quests/QuestsInitializers/NewBeginnings.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
-using UnityEngine.AddressableAssets;
+//using UnityEngine.AddressableAssets;
 
 [CreateAssetMenu(menuName = "ScriptableObjects/Quests/New Quest Initializer")]
 public class NewBeginnings : QuestInitialize
 {
-    public AssetReference syntheaBox;
+    //public AssetReference syntheaBox;
     public override void Initialize(int taskID)
     {
         if (taskID == 0)
         {
-            Addressables.InstantiateAsync(syntheaBox, StaticGameVariables._ITEMS);
+            //Addressables.InstantiateAsync(syntheaBox, StaticGameVariables._ITEMS);
         }
     }
 }
